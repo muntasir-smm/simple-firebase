a84a15b39170d4a45cd1f216640c0ba0dc3c4624
muntasir-smm <munna112ru@gmail.com>
2023-05-04 09:59:46 +0600

    first commit

---
+++ src/__steps.js
@@ -0,0 +1,9 @@
+/**
+ * 1. Visit: console.firebase.google.com
+ * 2. Create project (skip google analytics)
+ * 3. Register app (create config)
+ * 4. Install firebase
+ * 5. Add config file to your project
+ * 6. DANGER: Do not publish or make firebase config to public by pushing those to github
+ * 7. Go to Docs> Build > Authentication > Web > Get Started
+ */
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAfjYMqjjepGsoBM-x3A9l3VS38V9Idp-I",
+  authDomain: "simple-firebase-smm.firebaseapp.com",
+  projectId: "simple-firebase-smm",
+  storageBucket: "simple-firebase-smm.appspot.com",
+  messagingSenderId: "906968524373",
+  appId: "1:906968524373:web:8f03a185d2d0b9ebee96dc",
+  measurementId: "G-BGGKDLS3TL",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
