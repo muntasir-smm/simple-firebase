5f677e45d1fc4569c8264c481e75b1667d21d39f
muntasir-smm <munna112ru@gmail.com>
2023-05-04 10:41:15 +0600

    Login with Google

--- src/__steps.js
+++ src/__steps.js
@@ -2,8 +2,12 @@
  * 1. Visit: console.firebase.google.com
  * 2. Create project (skip google analytics)
  * 3. Register app (create config)
- * 4. Install firebase
+ * 4. Install firebase : npm install firebase
  * 5. Add config file to your project
  * 6. DANGER: Do not publish or make firebase config to public by pushing those to github
- * 7. Go to Docs> Build > Authentication > Web > Get Started
+ * 7. Go to Docs> Build > Authentication > Web > Get Started.
+ * 8. export app from the firebase.config.js file
+ * 9. Login.jsx: import getAuth from firebase/auth
+ * 10. create const auth=getAuth(app)
+ * 11. import googleAuthProvider and create a new provider
  */
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import app from "../../firebase/firebase.init";
 
 const Login = () => {
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    console.log("Google Loginnnnnnn");
+  };
+
   return (
     <div>
-      <h1>Login</h1>
+      <button onClick={handleGoogleSignIn}>Google Login</button>
     </div>
   );
 };
